8229fb873257d655831b4e2bb472ca97d94c6407
Paul Breuer <p.breuer@campus.tu-berlin.de>
2023-05-24 23:57:15 +0200

    feature: add solution attempt for pid.py
    * prediction is probably not 100% correct
    
    * robot becomes unresponsive after a 20-30 seconds

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,8 +35,8 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
+        self.Kp = 5
+        self.Ki = 0.1
         self.Kd = 0
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
@@ -53,6 +53,16 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
+        error_e = target - sensor             # tracking error
+        self.u = self.u + (self.Kp + self.Ki + self.Kd/self.dt) * error_e - (self.Kp + 2*self.Kd/self.dt) * self.e1 + self.Kd/self.dt * self.e2   # vl
+
+        self.e2 = self.e1
+        self.e1 = error_e
+
+        # PID control with prediction attempt
+        prediction_test = self.y[0] + self.u * self.dt
+
+        self.y.append(prediction_test)
 
         return self.u
 
